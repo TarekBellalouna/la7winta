13d125d2e19e8a7bd6877d3a58fa185f9b045ef5
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-03-21 22:01:32 +0100

    update

--- routes/api/profile.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router = express.Router();
-
-//@route get api/posts
-//@access public
-router.get('/', (req, res)=> res.send('posts route'));
-
-module.exports = router;
