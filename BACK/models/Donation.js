5f5acc66d78199bdf308b75b859208fcc701e99c
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-03-21 22:23:41 +0100

    update

--- BACK/models/Donation.js
+++ BACK/models/Donation.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const DonationSchema = new mongoose.Schema({
 
     title: {type: String, required: [true, "can't be blank"]},
-    picture: 
-    {   type: picture},
+
 
    
     user: [{ type: mongoose.Schema.Types.ObjectId, ref : "User"}] ,
